800c7eeee329d8e1c09959c65bc79f8f161bc2f6
matt-r-mck <matt.r.mck@gmail.com>
2020-07-30 12:36:30 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ɵisListLikeIterable  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  counter: number = 0;
+
+  nbrCls: string = "";
+
+display(): void{
+  this.nbrCls = "";
+  if(this.counter % 2 == 0){
+    this.nbrCls+="text-danger ";
+  }if(this.counter % 3 == 0){
+    this.nbrCls+="font-weight-bold ";
+  }if(this.counter % 5 == 0){
+    this.nbrCls+="font-italic ";
+  }
+}
+
+  increment (){
+    ++this.counter;
+    this.display();
+  }
+  decrement (){
+    --this.counter;
+    this.display();
+  }
+
   title = 'counter-angular';
 }
